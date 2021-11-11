3e15a19d163769b59cb8f78f43c58b39cc45178d
Brad Breiten <jonathan@breiten.us>
2021-11-11 14:54:26 -0700

    Add key class and tests

---
+++ lib/key.rb
@@ -0,0 +1,48 @@
+class Key
+
+  attr_reader :key,
+              :key_digits,
+              :a_shift,
+              :b_shift,
+              :c_shift,
+              :d_shift
+
+  def initialize(key)
+    @key = key
+    @key_digits = key.chars
+    @a_shift = a_shift
+    @b_shift = b_shift
+    @c_shift = c_shift
+    @d_shift = d_shift
+  end
+
+  def a_shift
+    shift = @key_digits[0] + @key_digits[1]
+    shift.to_i
+  end
+
+  def b_shift
+    shift = @key_digits[1] + @key_digits[2]
+    shift.to_i
+  end
+
+  def c_shift
+    shift = @key_digits[2] + @key_digits[3]
+    shift.to_i
+  end
+
+  def d_shift
+    shift = @key_digits[3] + @key_digits[4]
+    shift.to_i
+  end
+
+  def all_shifts
+    all_shifts = [a_shift, b_shift, c_shift, d_shift]
+  end
+
+  def generate_key_digits
+    key = ("0".."99999").to_a.sample
+    key.rjust(5, '0')
+  end
+
+end
---
+++ spec/key_spec.rb
@@ -0,0 +1,42 @@
+require "./lib/key"
+
+RSpec.describe Key do
+  let(:key_1) {Key.new("01234")}
+  let(:key_2) {Key.new}
+
+  it 'exists' do
+    expect(key_1).to be_a Key
+  end
+
+  it 'has attributes' do
+    expect(key_1.key_digits).to be_a Array
+    expect(key_1.key_digits.length).to eq 5
+    expect(("0".."99999")).to cover key_1.key
+  end
+
+  it 'has attributes by default' do
+    expect(key_2.key_digits).to be_a Array
+    expect(key_2.key_digits.length).to eq 5
+    expect(("0".."99999")).to cover key_2.key
+  end
+
+  describe '#shift methods' do
+    it 'returns an integer' do
+      expect(key_1.a_shift).to be_a Integer
+      expect(key_1.a_shift).to eq 1
+
+      expect(key_1.b_shift).to be_a Integer
+      expect(key_1.b_shift).to eq 12
+
+      expect(key_1.c_shift).to be_a Integer
+      expect(key_1.c_shift).to eq 23
+
+      expect(key_1.d_shift).to be_a Integer
+      expect(key_1.d_shift).to eq 34
+    end
+
+    it 'has array of shifts' do
+      expect(key_1.all_shifts).to eq([1, 12, 23, 34])
+    end
+  end
+end
