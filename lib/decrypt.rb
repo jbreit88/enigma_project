f5384676f290f8315070daa855610c8c01074158
Brad Breiten <jonathan@breiten.us>
2021-11-13 12:15:48 -0700

    Start runner files

---
+++ lib/decrypt.rb
@@ -0,0 +1,8 @@
+#four command line arguments
+
+#existing file with encrypted message
+#file to write decrypted message
+#decryption key
+#date used for decryption
+
+#output to screen filename, key, date.
---
+++ lib/encrypt.rb
@@ -0,0 +1,25 @@
+require 'date'
+require './lib/enigma'
+
+
+message
+def encrypt_message(message, encrypted)
+
+  message_text = File.open(message)
+
+  message_text.read
+  #open file
+  #get text
+  #pass text as message arg
+  #use rand key and todays date
+
+end
+
+encrypt_message
+#two command line arguments
+
+#take in existing txt with message to encrypt
+
+#write encrypted message to new txt file.
+
+#Output to screen file name, key, and date.
