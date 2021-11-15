b6bccb19e8ba3283f3f2a5c0aa609e6140c9f253
Brad Breiten <jonathan@breiten.us>
2021-11-15 11:54:13 -0700

    Add tests for intialize to translator.

--- spec/translator_spec.rb
+++ spec/translator_spec.rb
@@ -2,10 +2,22 @@ require './lib/translator'
 
 
 RSpec.describe Translator do
-  let(:translator) {Translator.new(charcter_set, message, shift_values, direction)}
+  let(:character_set) {["a".."z"] << " "}
+  let(:message) {"Hello World!"}
+  let(:shift_values) {[1, 2, 3, 4]}
+  let(:direction_1) {"forward"}
+  let(:direction_2) {"backward"}
+  let(:translator) {Translator.new(character_set, message, shift_values, direction_1)}
 
   it 'exists' do
     expect(translator).to be_a Translator
+  end
+
+  it 'has attributes' do
+    expect(translator.character_set).to eq(['a'..'z'] << " ")
+    expect(translator.message).to eq "Hello World!"
+    expect(translator.shift_values).to eq([1, 2, 3, 4])
+    expect(translator.direction).to eq "forward"
   end
 
 
